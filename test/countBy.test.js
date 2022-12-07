b0f56e52e14330aba22c16baa6813b02e0bc8a62
Iida Kainu <iida.kainu@vincit.fi>
2022-12-07 13:21:42 +0200

    Refactor tests
    Group it()-statements with describe() and rename test descriptions
    Define commonly used variables in the beginning of the file.

--- test/countBy.test.js
+++ test/countBy.test.js
@@ -1,52 +1,41 @@
 import countBy from '../src/countBy.js';
 
+const users = [
+  { user: 'barney', active: true },
+  { user: 'betty', active: true },
+  { user: 'fred', active: false }
+];
+
 describe('countBy', () => {
-  it('can correctly count attributes from an array of objects', () => {
-    const users = [
-      { user: 'barney', active: true },
-      { user: 'betty', active: true },
-      { user: 'fred', active: false }
-    ];
+  it('counts attributes from an array of objects', () => {
+    const isActive = ({ active }) => active;
 
-    expect(countBy(users, value => value.active)).toBe({ true: 2, false: 1 });
+    expect(countBy(users, isActive)).toBe({ true: 2, false: 1 });
   });
-  it('can correctly iterate attributes of object', () => {
-    const user = { user: 'barney', active: true };
+  it('iterates attributes of object', () => {
+    const isString = value => typeof value === 'string';
 
-    expect(countBy(user, value => typeof value === 'string')).toBe({
-      true: 1,
-      false: 1
-    });
+    expect(countBy(users[0], isString)).toBe({ true: 1, false: 1 });
   });
-  it('can correctly iterate an empty array', () => {
+  it('iterates an empty array', () => {
     const emptyArray = [];
 
     expect(countBy(emptyArray, value => value)).toStrictEqual({});
   });
-  it('can correctly iterate an empty object', () => {
+  it('iterates an empty object', () => {
     const emptyObject = {};
 
     expect(countBy(emptyObject, value => value)).toStrictEqual({});
   });
-  it('can handle a function that does not return anything', () => {
-    const users = [
-      { user: 'barney', active: true },
-      { user: 'betty', active: true },
-      { user: 'fred', active: false }
-    ];
-
-    expect(countBy(users, value => {})).toBe({ undefined: 3 });
+  it('handles a function that does not return anything', () => {
+    expect(countBy(users, () => {})).toBe({ undefined: 3 });
   });
-  it('throws TypeError if first argument is not an array or object', () => {
-    expect(() => countBy(true, value => value)).toThrow(TypeError);
-  });
-  it('throws TypeError if second argument is not a function', () => {
-    const users = [
-      { user: 'barney', active: true },
-      { user: 'betty', active: true },
-      { user: 'fred', active: false }
-    ];
-
-    expect(() => countBy(users, true)).toThrow(TypeError);
+  describe('handles exceptions', () => {
+    it('throws TypeError if first argument is not an array or object', () => {
+      expect(() => countBy(true, value => value)).toThrow(TypeError);
+    });
+    it('throws TypeError if second argument is not a function', () => {
+      expect(() => countBy(users, true)).toThrow(TypeError);
+    });
   });
 });
